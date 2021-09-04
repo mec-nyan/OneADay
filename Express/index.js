41c35ef2b74b3c2c7d185d5da6924c1ce3ddfb07
mec-nyan <marianoraspberrypi@gmail.com>
2021-09-04 16:19:25 -0300

    new route

--- Express/index.js
+++ Express/index.js
@@ -6,6 +6,21 @@ app.get('/', (req, res) => {
   res.send('Hello Express!');
 });
 
+app.get('/api', (req, res) => {
+  const metalGod = 'Rob Halford';
+  const judasPriest = {
+    vocals: metalGod,
+    guitars: {
+      rhythm: 'K.K. Downing',
+      lead: 'Glenn Tipton',
+    },
+    bass: 'Ian Hill',
+    drums: 'Scott Travis',
+  };
+
+  res.status(201).json(judasPriest);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
