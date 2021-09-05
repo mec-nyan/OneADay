72f4eee7144f1deeea5cd0a5e55abf42a9ddb3ae
mec-nyan <marianoraspberrypi@gmail.com>
2021-09-04 21:37:44 -0300

    Added judaspriest API

--- Express/index.js
+++ Express/index.js
@@ -1,24 +1,24 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 3000;
 
+const { metalGod, judasPriest } = require('./judaspriest');
 app.get('/', (req, res) => {
   res.send('Hello Express!');
 });
 
 app.get('/api', (req, res) => {
-  const metalGod = 'Rob Halford';
-  const judasPriest = {
-    vocals: metalGod,
-    guitars: {
-      rhythm: 'K.K. Downing',
-      lead: 'Glenn Tipton',
-    },
-    bass: 'Ian Hill',
-    drums: 'Scott Travis',
-  };
-
   res.status(201).json(judasPriest);
+});
+
+app.get('/api/:name', (req, res) => {
+  let { name } = req.params;
+  if (name) {
+    name = name.replace('%20', ' ');
+    // Process further modifications here
+  }
+  let out = judasPriest.members[name] || {};
+  res.status(200).json(out);
 });
 
 app.listen(port, () => {
---
+++ Express/judaspriest.js
@@ -0,0 +1,54 @@
+// The JP Api!
+
+const metalGod = 'Rob Halford';
+
+const judasPriest = {
+  summary: 'Best Heavy Metal band ever!',
+  origin: 'Birmingham, England',
+  years: '1969-present',
+  genre: 'Heavy Metal',
+  website: 'www.judaspriest.com',
+  members: {
+    'Rob Halford': {
+      fullName: 'Robert John Arthur Halford',
+      birthday: '25 August 1951',
+      origin: 'Walsall, England',
+      instrument: 'vocals',
+    },
+    'Ian Hill': {
+      fullName: 'Ian Frank Hill',
+      birthday: '20 January 1951',
+      origin: 'West Bromwich, England',
+      instrument: 'bass',
+    },
+    'Glenn Tipton': {
+      fullName: 'Glenn Raymond Tipton',
+      birthday: '25 October 1947',
+      origin: 'Blackheath, England',
+      instrument: 'guitars',
+    },
+    'K.K. Downing': {
+      fullName: 'Kenneth Keith Downing Jr.',
+      birthday: '27 October 1951',
+      origin: 'West Bromwich, Staffordshire, England',
+      instrument: 'guitars',
+    },
+    'Scott Travis': {
+      fullName: 'Mark Scott Travis', 
+      birthday: '6 September 1961', 
+      origin: 'Norfolk, Virginia, US',
+      instrument: 'drums',
+    },
+    'Richie Faulkner': {
+      fullName: 'Richard Ian Faulkner', 
+      birthday: '1 January 1980', 
+      origin: 'London, England',
+      instrument: 'guitars',
+    },
+  },
+}
+
+module.exports = {
+  metalGod,
+  judasPriest,
+}
