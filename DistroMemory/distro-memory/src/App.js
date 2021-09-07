5282870b5d7b3b6eff78c8137abf6a6b51d0c308
mec-nyan <marianoraspberrypi@gmail.com>
2021-09-06 23:42:21 -0300

    modify App.js

--- DistroMemory/distro-memory/src/App.js
+++ DistroMemory/distro-memory/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+import FancyButton from './fancyButton';
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <FancyButton />
     </div>
   );
 }
