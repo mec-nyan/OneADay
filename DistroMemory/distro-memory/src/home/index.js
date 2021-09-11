9fef390377d918a9bea8dc27d48c0f9f08d913a2
mec-nyan <marianoraspberrypi@gmail.com>
2021-09-11 02:03:29 -0300

    Home styles, palette, css background

--- DistroMemory/distro-memory/src/home/index.js
+++ DistroMemory/distro-memory/src/home/index.js
@@ -1,12 +1,19 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 import './home.css';
+
+
+const star = <FontAwesomeIcon icon={faStar}/>;
 
 export default function Home() {
 
   return (
     <div className='home'>
-      <button className='startBtn'>Start</button>
+      <button className='startBtn'>
+        <span className='rotate'>{star}</span>
+        <span>Start</span>
+        <span className='rotate'>{star}</span>
+      </button>
     </div> 
   );
 }
