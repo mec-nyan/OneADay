e7afc7f25e7b6ec3422048487fd1a49c927ce4c4
mec-nyan <marianoraspberrypi@gmail.com>
2021-09-13 03:31:55 -0300

    added logos

--- DistroMemory/distro-memory/src/play/board/index.js
+++ DistroMemory/distro-memory/src/play/board/index.js
@@ -1,18 +1,36 @@
 import Tile from './tile';
 import gnu from './img/heckert_gnu.transp.small.png';
 import ubuntu from './img/cof_orange_hex.png';
+import debian from './img/openlogo-nd.png';
+import arch from './img/arch.png';
+import fedora from './img/fedora.png';
+import centos from './img/centos.png';
+import slack from './img/slackware.png';
 import './board.css';
 
 
 export default function Board({ level }) {
-  const tiles = [];
-  for (let i = 0; i < 24; ++i) {
-    tiles.push(<Tile back={gnu} front={ubuntu} />);
-  }
+  let logos = [ ubuntu, debian, arch, fedora, centos, slack ];
+  logos = [ ...logos, ...logos, ...logos, ...logos ];
+  let tiles = [];
+  tiles = logos.map(logo => <Tile back={gnu} front={logo} />);
+  shuffle(tiles);
 
   return (
     <div className='board'>
       {tiles}
     </div> 
   );
+}
+
+
+function shuffle(arr) {
+  console.log(arr);
+  let element;
+  for (let i = 0; i < arr.length; ++i) {
+    let j = Math.floor(Math.random() * arr.length);
+    [ element ] = arr.splice(j, 1);
+    arr.push(element);
+  }
+  console.log(arr);
 }
