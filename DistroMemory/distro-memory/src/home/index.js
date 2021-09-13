fed4fdd3c027ff23896922c310e992109691ca83
mec-nyan <marianoraspberrypi@gmail.com>
2021-09-12 23:15:52 -0300

    nav to home

--- DistroMemory/distro-memory/src/home/index.js
+++ DistroMemory/distro-memory/src/home/index.js
@@ -1,17 +1,20 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { Link, NavLink } from 'react-router-dom';
 import Nav from '../nav';
 import './home.css';
 
 
 const star = <FontAwesomeIcon icon={faStar}/>;
+const bars = <FontAwesomeIcon icon={faBars}/>;
+const exit = <FontAwesomeIcon icon={faBars}/>;
 
 export default function Home() {
 
   return (
     <div className='home'>
-      <Nav />
+      <Nav left={bars} center='Distro memory' bars={bars}/>
       <NavLink className='startBtn' to='/board'>
         <span className='rotate'>{star}</span>
         <span>Start</span>
